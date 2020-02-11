a8dee2b8825162d46f185f710a6d6c1745605f0d
syki66 <66syki@gmail.com>
2020-02-11 19:50:39 +0900

    apply slide

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import { Toolbar, AppBar } from '@material-ui/core';
+
+
+const photos = [
+  {
+    name: 'Photo 1',
+    url: "http://www.suvincos.com/sh_img/index/main_banner/main_banner_01.png"
+  },
+  {
+    name: 'Photo 2',
+    url: "http://www.suvincos.com/sh_img/index/main_banner/main_banner_02.png"
+  }
+]
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+
+          </Toolbar>
+        </AppBar>
+
+        <div style={{ display: 'flex', justifyContent: 'center', height: '100vh' }}>
+
+
+
+
+          <Carousel
+            infiniteLoop={true}
+            autoPlay={true}
+            width={2000}
+            showStatus={false}
+            showThumbs={false}
+
+
+          >
+            <div>
+              <img src="http://www.suvincos.com/sh_img/index/main_banner/main_banner_01.png" alt="pic01" />
+
+            </div>
+            <div>
+              <img src="http://www.suvincos.com/sh_img/index/main_banner/main_banner_02.png" alt="pic02" />
+
+            </div>
+          </Carousel>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
