2d4f88d81ef49b369f24b55ced19fa37eab8132b
syki66 <66syki@gmail.com>
2020-02-23 02:08:25 +0900

    googlemap added

--- src/routes/Information.js
+++ src/routes/Information.js
@@ -1,7 +1,39 @@
-import React from "react";
 
-function Information() {
-    return <span>information js 임....</span>
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+
+
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Information extends Component {
+  static defaultProps = {
+    center: {
+      lat: 37.4965131,
+      lng: 126.7865525
+    },
+    zoom: 19
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={37.4965131}
+            lng={126.7865539}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
 
 export default Information;
