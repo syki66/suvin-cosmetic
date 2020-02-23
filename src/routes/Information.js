1cf019e57501c60a3cd00f94f07f3bd694deb4e0
syki66 <66syki@gmail.com>
2020-02-23 17:11:00 +0900

    env test

--- src/routes/Information.js
+++ src/routes/Information.js
@@ -1,39 +1,24 @@
 
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps'
 
 
-
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-class Information extends Component {
-  static defaultProps = {
-    center: {
-      lat: 37.4965131,
-      lng: 126.7865525
-    },
-    zoom: 19
-  };
-
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={37.4965131}
-            lng={126.7865539}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
+function Information() {
+  return (
+    <RenderAfterNavermapsLoaded
+      ncpClientId={process.env.REACT_APP_API}
+    >
+      <NaverMap
+        mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+        style={{
+          width: '100%',
+          height: '400px',
+        }}
+        defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
+        defaultZoom={10}
+      />
+    </RenderAfterNavermapsLoaded>
+  );
 }
 
 export default Information;
