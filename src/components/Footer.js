65ae904651c06bb0e50e076abd581e6ed5111c4b
syki66 <66syki@gmail.com>
2020-03-05 22:33:53 +0900

    modify footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,52 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBIcon, MDBBtn } from "mdbreact";
 
 function Footer() {
     return (
         <MDBFooter color="elegant-color" >
             <MDBContainer >
-                <MDBRow >
-                    <MDBCol >
-                        <p>
-                            <span>[14566] 경기도 부천시 부천로136번길27 원미어울마당406호 Tel : 1688-9028 / 010-6644-3665</span><br />
-                            <span>E-mail : dhkkis@daum.net 사업자 번호 : 130-42-62423 상호 : 수빈코스메틱 대표 : 김덕훈</span>
-                        </p>
-                    </MDBCol>
-                    <MDBCol >
-                        <p> 오른쪽에 버튼만들 예정</p>
 
+                <MDBRow className="p-2 font-small">
+
+                    <MDBCol size="12" md="11">
+                        <div>
+                            <div className="footer_display">[14566] 경기도 부천시 부천로136번길27 원미어울마당406호 </div>
+                            <div className="footer_display">Tel : 1688-9028 / 010-6644-3665 </div>
+                        </div>
+                        <div>
+                            <div className="footer_display">E-mail : dhkkis@daum.net </div>
+                            <div className="footer_display">사업자 번호 : 130-42-62423</div>
+                            <div className="footer_display">상호 : 수빈코스메틱, 대표 : 김덕훈</div>
+                        </div>
                     </MDBCol>
+
+                    <MDBCol size="12" md="1">
+                        <div style={{backgroundColor:"red"}}>소셜 버튼</div>
+                    </MDBCol>
+
                 </MDBRow>
+
             </MDBContainer>
-            <div >
-                <MDBContainer >
+            
+
+
+
+            <div className="footer-copyright text-center py-1">
+                <MDBContainer fluid>
                     Copyright ⓒ <a href="http://www.suvincos.com/"> www.suvincos.com </a> All rights reserved.
-          </MDBContainer>
+                </MDBContainer>
             </div>
+
         </MDBFooter>
+
+
+
+
+
+
+
+
+
     );
 
 }
