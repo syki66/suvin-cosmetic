7a084bbc245e5330d31a03779df3574085b3cbf9
syki66 <66syki@gmail.com>
2020-03-07 17:09:17 +0900

    company page css box finished

--- src/components/Information_Components/Company.js
+++ src/components/Information_Components/Company.js
@@ -10,8 +10,7 @@ import SideNav from "./SideNav";
     먼저 div 하다 더 만들어서 싹 감싸고서 부모를 relative 자식을 absolute로 두고서 박스 두개 겹쳐서 했음 zIndex가 깊이 조정
     삼각형 사각형 원만 이용해서 만들수 있는듯
 
-
-    가로선그은거 창벌리면 미세하게 오른쪽 달라지는거 해결하기
+    글 줄바꾸는거는 br태그에다가 display block none 이거 써서 해결
  */
 
 function Company() {
@@ -75,8 +74,8 @@ function Company() {
                                                 borderLeft: "150px solid white",
                                                 borderTop: "190px solid #f0e2e8",
 
-                                                width:"0px",
-                                                height:"0px",
+                                                width:"100%",
+                                                height:"100%",
                                                 flex:"1"
                                                 
                                             }}
@@ -88,10 +87,10 @@ function Company() {
                                         <div
                                             style={{
                                                 borderLeft: "170px solid #f0e2e8",
-                                                borderTop: "240px solid transparent",
+                                                borderTop: "237px solid transparent",
 
-                                                width:"0px",
-                                                height:"0px",
+                                                width:"0",
+                                                height:"100%",
                                                 flex:"1"
                                             }}
                                         ></div>
@@ -104,7 +103,7 @@ function Company() {
 
 
 
-                                <ul className="py-4" style={{ backgroundColor: "transparent" }}>
+                                <ul className="py-4 mb-0" style={{ backgroundColor: "transparent" }}>
                                     <div className="h5 py-1" style={{ color: "#444444" }}><b>아름다움을 위한 기술개발과 가치창출</b></div>
                                     <ol>
                                         <li>Competence on technology</li>
@@ -135,16 +134,19 @@ function Company() {
                     <div className="h5 py-1" style={{ color: "#7f304e" }}><b>최고 품질의 제품으로 고객과 함께 새롭게 성장하는 수빈코스메틱</b></div>
 
 
-                    <MDBRow className="pb-5">
+                    <MDBRow className="">
                         <MDBCol size="12" className="">
 
                             <div style={{position:"relative", zIndex:"1"}}>
 
                                 <div className="cssShapeBox_disappear" style={{display:"none"}}>
+                                    
+                                    
+                                    
                                     <div
                                         style={{
-                                            display:"flex",
-                                            flexDirection:"column",
+                                            display:"grid",
+                                            gridTemplateRows:"1fr 1fr 1fr",
                                             position: "absolute",
                                             zIndex:"-1",
                                             width:"100%",
@@ -152,22 +154,20 @@ function Company() {
                                     }}>
 
 
+                                        <div style={{backgroundColor:"transparent"}}></div>
 
 
-                                    <div style={{flex:"1", backgroundColor:"transparent"}}></div>
+                                        <div style={{
+                                            borderLeft:"848px solid transparent",
+                                            borderBottom: "86px solid #f4f4f4",
+                                            
+                                            width:"100%",
+                                            height:"100%",
+                                        }}></div>
 
-                                        <div style={{flex:"1", display:"flex"}}>
-                                            <div style={{
-                                                flex:"1",
 
-                                                borderRight: "848px solid #f4f4f4",
-                                                borderTop: "88px solid transparent",
-
-                                                width:"0px",
-                                                height:"0px"
-                                                }}></div>
-                                        </div>
-                                        <div style={{flex:"1", backgroundColor:"#f4f4f4"}}></div>
+                                        <div style={{backgroundColor:"#f4f4f4"}}></div>
+                                        
 
                                     </div>
                                 </div>
@@ -179,15 +179,15 @@ function Company() {
 
 
                                 <div>
-                                    <div>
-                                        Founded on the base of the patented technology about pore care by President Kim Duckhoon, a 30-year cosmetic R&D 
-                                        specialist, Suvin has presently a wide and competitive product line, global network and diverse<br /> distribution
-                                        Channel. Future development and prosperity is expected with<br /> faithful customers
-                                        and product reputation on market.
+                                    <div className="pb-3 pb-xl-0">
+                                        Founded on the base of the patented technology about pore care by President Kim Duckhoon, a 30-year cosmetic R&D <br style={{display:"none"}} className="br__appear"/>
+                                        specialist, Suvin has presently a wide and competitive product line, global network and diverse<br style={{display:"none"}} className="br__appear"/>
+                                        distribution Channel. Future development and prosperity is expected with<br style={{display:"none"}} className="br__appear"/>
+                                        faithful customers and product reputation on market.
                                     </div>
-                                    <div className="px-4 py-4 border border-light py-xl-5 after_appear_cssShapeBox" style={{backgroundColor:"#f4f4f4"}}>
-                                        모공관리 특허기술을 기반으로 설립한<br />
-                                        수빈코스메틱은 경쟁력 있는 다양한 제품들을 여러 판매망를 통하여<br />
+                                    <div className="p-4 py-xl-5 border border-bottom-0 border-light after_appear_cssShapeBox" style={{backgroundColor:"#f4f4f4"}}>
+                                        모공관리 특허기술을 기반으로 설립한<br style={{display:"none"}} className="br__appear"/>
+                                        수빈코스메틱은 경쟁력 있는 다양한 제품들을 여러 판매망를 통하여<br style={{display:"none"}} className="br__appear"/>
                                         시장에 선보이고 있으며 고품질 제품의 신뢰를 바탕으로 항상 고객과 함께하고 있습니다.
                                     </div>
                                 </div>
