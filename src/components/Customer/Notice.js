1b2cba462704b7deecc7ae88df81d7aa3d5af7d8
syki66 <66syki@gmail.com>
2020-03-13 14:28:59 +0900

    fix notice page error

--- src/components/Customer/Notice.js
+++ src/components/Customer/Notice.js
@@ -10,34 +10,28 @@ const markdownFiles = importAll(require.context('../../posts/', false, /\.md$/))
 
 /*
     posts 폴더 전체를 임포트 시켜서 각 md파일의 내용을 fetch 이용해서 가져와서 state의 posts 어레이에 저장함
-
     그후 render에서 posts 어레이 이용해서 규격에 넣어서 화면상에 출력
-
     escapeHTML은 false로 두면 html 사용 가능해짐
-
     댓글 디스커스 쓸수있게 해야겠다.
-
     front matter는 아래형식에 맞춰서 date와 title 집어넣어줘야됨(순서는 상관없음). 띄어쓰기와 오타 주의 글의 시작은 front matter 와 한칸 띄워서 시작하기
-    
+    비동기에 있어서 isLoading 은 필수
 */
 
 
 /*  마크다운 front matter 형식
-
     ---
     date: "2020-03-12"
     title: "제목입니다."
     ---
-
     여기서부터 글 시작
-
 */
 
  
 export default class Notice extends React.Component {
 
     state = {
-        posts: [],
+        isLoading: true,
+        posts: []
     }
 
 
@@ -54,9 +48,6 @@ export default class Notice extends React.Component {
         return {date, title, mainText}
     }
 
-    loadMarkdown = () => {
-        
-    }
 
 
     async componentDidMount() {
@@ -68,21 +59,13 @@ export default class Notice extends React.Component {
                 )
         ).catch((err) => console.error(err));
 
-
-        // const posts = await markdownArray.map((each) => {
-        //     console.log(this.sliceFrontMatter(each))
-        // })
-
-        console.log(posts[0].title)
-        this.setState({ posts });
+        this.setState({ posts, isLoading: false });
     }
 
 
 
     render() {
-        const { posts } = this.state;
-        
-
+        const { posts, isLoading } = this.state;
         
 
         return (
@@ -92,11 +75,15 @@ export default class Notice extends React.Component {
             >
                 <div className="h2 py-2">Notice</div>
                 
-                <Markdown 
-                    source={posts} 
-                    className="border-top border-light pt-5"
-                    escapeHtml={false}
-                />
+                {isLoading ? (
+                    <div>데이터 로딩중</div>
+                    ) : (
+                        <Markdown 
+                            source={posts[0].mainText} 
+                            className="border-top border-light pt-5"
+                            escapeHtml={false}
+                        />
+                ) }
             </InnerPageFrame>
         );
     }
